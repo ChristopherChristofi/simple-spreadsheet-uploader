11588d503dd9b6a4be653fe7f5d8c9f2e2949ade
christopher-christofi <christopherlchristofi@outlook.com>
2021-05-03 20:02:12 +0100

    Initial server config, added file post method

--- app/app.js
+++ app/app.js
@@ -2,31 +2,68 @@
 
 const express = require("express");
 
-const path = require("path");
+const path = require('path');
 
-const app = express();
+const multer = require('multer');
 
 require("dotenv").config();
 
+const app = express();
+
+let storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './data/uploads');
+    },
+    filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        // TODO improve method of setting filetype as csv
+        cb(null, file.fieldname + '-' + uniqueSuffix + '.csv');
+    },
+    fileFilter: (req, file, cb) => {
+        let extension = path.extname(file.originalname);
+        if (extension !== '.csv' && extension !== 'xlsx') {
+            return cb(new Error('Only speadsheet files are allowed'))
+        }
+        cb(null, true)
+    }
+});
+
+let upload = multer({
+    storage: storage
+});
+
+
 let host = process.env.HOST || "localhost";
 let port = process.env.PORT || 3000;
+
+let static_dir = {
+    root: path.join(__dirname, '../public/')
+};
 
 async function run () {
 
     try {
 
-        app.use(express.static("static"));
+        app.use(express.static("public"));
 
         app.get("/", (req, res) => {
-            res.sendFile("upload.html", { root: path.join(__dirname, '../static/')});
+            res.sendFile("upload.html", static_dir);
         });
 
         app.get("*", (req, res) => {
-            res.sendFile("404.html", { root: path.join(__dirname, '../static/')});
+            res.sendFile("404.html", static_dir);
+        });
+
+        app.post('/api/upload', upload.single('spreadsheet'), (req, res, err) => {
+            if (err) {
+                return console.error(err.message);
+            }
+            res.send("OK");
+            return console.log("File Uploaded.")
         });
 
         app.listen(port, host, () => {
-            console.log(`Server listening on ${host}:${port}`);
+            console.log(`Server working on ${host}:${port}`);
         });
 
     } catch (err) {
